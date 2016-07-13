640c2c5cfd9f18af69035840676cf57355e9ed96
Scanna <luca.scannapieco@mail.polimi.it>
2016-07-13 21:54:39 +0200

    faq smart life

--- myWebsite/includes/js/smart-life.js
+++ myWebsite/includes/js/smart-life.js
@@ -10,20 +10,42 @@ $("document").ready(
             
             success: function(response) { 
                 
-                console.log(response);
+                //put the query result in an array of js objects
                 var smartlife=JSON.parse(response);
                 
+                //setting orientation info
+                var categoryInfo=document.getElementById("category-info");
+                var urlCategoryInfo="smart-life-by-category.html?idcategory=" +smartlife[0].idcategoria;
+                var categoryInfoLink=document.createElement("a");
+                categoryInfoLink.setAttribute("href",urlCategoryInfo);
+                var categoryInfoName=document.createTextNode(smartlife[0].nomecategoria);
+                categoryInfoLink.appendChild(categoryInfoName);
+                categoryInfo.appendChild(categoryInfoLink);
+                var smartlifeInfo=document.getElementById("smartlife-info");
+                var smartlifeInfoName=document.createTextNode(smartlife[0].nomesmartlife);
+                smartlifeInfo.appendChild(smartlifeInfoName);
                 
-
-                var container=document.getElementById("main");
+                //setting the faq link
+                var faqLink=document.getElementById("faq-link");
+                var faqurl="smart-life-faq.html?idsmartlife="+smartlife[0].idsmartlife;
+                faqLink.setAttribute("href", faqurl);
                 
-                
-                var img=document.createElement("img");
+                //setting main image of the page
+                var img=document.getElementById("big-image");
                 img.setAttribute("src","images/big-"+smartlife[0].fotosmartlife);
                 
-                container.appendChild(img);
+                //setting description and activation text
+                var headerDescription=document.getElementById("header-description");
+                var text=document.createTextNode(smartlife[0].nomesmartlife);
+                headerDescription.appendChild(text);
+                var textDescription=document.getElementById("text-description");
+                text=document.createTextNode(smartlife[0].descrizionelongsmartlife);
+                textDescription.appendChild(text);
+                var textActivation=document.getElementById("text-activation");
+                text=document.createTextNode(smartlife[0].regoleattivazionesmartlife);
+                textActivation.appendChild(text);
                 
-                
+                          
             }, 
             error: function(request,error) { 
                 console.log(request+":"+error);
