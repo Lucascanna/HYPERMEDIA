ef3cd257fbeeca84aac9a744a9f4fa0ecdee579d
Scanna <luca.scannapieco@mail.polimi.it>
2016-07-14 18:09:03 +0200

    all-assistance-by-category

---
+++ myWebsite/includes/js/all-assistance-by-category.js
@@ -0,0 +1,65 @@
+$("document").ready(
+    function(){ 
+ 
+        $.ajax({ 
+            method: "POST", 
+            crossDomain: true, 
+            url: "includes/php/query.php", //Relative or absolute path to file.phpfile  
+            
+            data: {query:   "SELECT * "+
+                            "FROM categorieassistenza "+
+                            "ORDER BY idcategoria ASC "  },
+            
+            success: function(response) { 
+                var categories=JSON.parse(response);
+
+                var container=document.getElementById("main");
+                
+                var i=0;
+                for(i=0;i<categories.length;i++){
+                    
+                    //setting links
+                    var urlCategory="assistance-by-category.html?idcategory="+categories[i].idcategoria;
+                    
+                    //putting a row for each category
+                    var row=document.createElement("div");
+                    row.setAttribute("class","row");
+                    row.setAttribute("style", "margin-top:20px margin-right:0");
+                    container.appendChild(row);
+                    var link=document.createElement("a");
+                    link.setAttribute("href",urlCategory);
+                    row.appendChild(link);
+                    var coloumnImg=document.createElement("div");
+                    coloumnImg.setAttribute("class", "col-sm-1");
+                    link.appendChild(coloumnImg);
+                    var img=document.createElement("img");
+                    img.setAttribute("class", "img-responsive");
+                    img.setAttribute("src", "images/"+categories[i].fotocategoria);
+                    img.setAttribute("style", "margin-top: 21px")
+                    coloumnImg.appendChild(img);
+                    var coloumnText=document.createElement("div");
+                    coloumnText.setAttribute("class", "col-sm-9");
+                    link.appendChild(coloumnText);
+                    var title=document.createElement("h3");
+                    var titleText=document.createTextNode(categories[i].nomecategoria);
+                    var titleBold=document.createElement("b");
+                    titleBold.appendChild(titleText);
+                    title.appendChild(titleBold);
+                    coloumnText.appendChild(title);
+                    var description=document.createElement("h4");
+                    var descriptionText=document.createTextNode(categories[i].descrizionecategoria);
+                    description.appendChild(descriptionText);
+                    coloumnText.appendChild(description);
+
+                }
+                    
+                    
+                
+                
+            }, 
+            error: function(request,error) { 
+                console.log(request+":"+error);
+            } 
+        }); 
+        return false; 
+});
