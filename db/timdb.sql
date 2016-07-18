3457510d44f0bcab340148612035aceaa6b19c26
Scanna <luca.scannapieco@mail.polimi.it>
2016-07-18 17:29:05 +0200

    fix

--- db/timdb.sql
+++ db/timdb.sql
@@ -20,8 +20,8 @@ SET time_zone = "+00:00";
 -- Database: `timdb`
 --
 
-CREATE DATABASE timdb;
-USE timdb;
+
+USE my_pencoscannapiecotorelli;
 
 -- --------------------------------------------------------
 
